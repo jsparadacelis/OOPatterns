613ff4d02f5a3bf2fdf90dddf46360d2aebd258b
jsparadacelis <parada.celis@gmail.com>
2020-08-05 23:45:20 -0500

    Add abstract factory exmaple

--- creational/main.go
+++ creational/main.go
@@ -6,4 +6,17 @@ func main() {
 	animalFactory := AnimalFactory{}
 	myAnimal := animalFactory.getAnimal("dog")
 	fmt.Println("Animal sound: ", myAnimal.getSound())
+
+	/* So, we need to call our abstract factory to create
+	some objects. Go ahead
+	*/
+	bookFactory := BookFactory{}
+	myBook := bookFactory.createBook("spanish", "fantasy")
+	fmt.Println(myBook.myNameIs())
+	myBook = bookFactory.createBook("portuguese", "fantasy")
+	fmt.Println(myBook.myNameIs())
+	myBook = bookFactory.createBook("spanish", "drama")
+	fmt.Println(myBook.myNameIs())
+	myBook = bookFactory.createBook("portuguese", "drama")
+	fmt.Println(myBook.myNameIs())
 }
